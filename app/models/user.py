913dc4dc977d53f174f7f23b866546823a25cbd4
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-20 18:24:23 -0600

    imported relationship from sqlalchemy.orm and added repos variable to user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.db.base import Base # <-- imported Base fromn db/base.py
 
 class User(Base):
@@ -9,3 +10,5 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    repos = relationship("Repo", back_populates="owner", cascade="all, delete-orphan")

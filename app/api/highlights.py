074a94d4943fb77bf13243e75fdbc6a0358d360b
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-20 22:11:50 -0600

    Fixed bug for highlights.py in api folder...because get_db was not in db/sessions.py, it was in: api/auth.py instead

--- app/api/highlights.py
+++ app/api/highlights.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from app.db.session import get_db
+from app.api.auth import get_db
 from app.models.highlight import Highlight
 from app.schemas.highlight import HighlightCreate, HighlightResponse
 

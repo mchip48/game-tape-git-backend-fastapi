e156e59d261bb6c524be94a480ae94c1179f08d6
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-20 19:04:23 -0600

    updated base.py to include Base class and pass in the inner code block

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,9 @@
-from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import declarative_base
 
-Base = declarative_base()
+# Base = declarative_base()
+
+# app/db/base.py
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass

be0a508e2b47a11424f326aea2735a95091389ba
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-20 21:18:13 -0600

    Updated app/schemas/commit.py to match models/commit.py

--- app/schemas/commit.py
+++ app/schemas/commit.py
@@ -1,16 +1,23 @@
 # app/schemas/commit.py
 from pydantic import BaseModel, HttpUrl
 from datetime import datetime
+from typing import Optional
 
 class CommitCreate(BaseModel):
     sha: str
     message: str
     author: str
+    date: Optional[datetime] = None
     url: HttpUrl
+    repo_id: int
 
-class CommitResponse(CommitCreate):
+class CommitResponse(BaseModel):
     id: int
+    sha: str
+    message: str
+    author: str
     date: datetime
+    url: str
     repo_id: int
 
     class Config:

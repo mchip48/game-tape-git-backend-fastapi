8fadad1c8169b77ca419f7a029e29773adebcb49
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-21 21:55:47 -0600

    Added github_analysis.py to /core with summarize_commits function

---
+++ app/core/github_analysis.py
@@ -0,0 +1,21 @@
+# app/core/github_analysis.py
+from typing import List
+
+async def summarize_commits(commits: List[dict]):
+    """
+    Summarize GitHub commits for a repo.
+    Input: list of commit JSON objects from GitHub API
+    Output: list of dicts with key info (author, date, message snippet, files changed)
+    """
+    highlights = []
+    for commit in commits:
+        commit_info = commit.get("commit", {})
+        author_info = commit_info.get("author", {})
+        highlights.append({
+            "author": author_info.get("name"),
+            "date": author_info.get("date"),
+            "message": commit_info.get("message", "")[:50],  # snippet
+            # 'files' key may not exist in GitHub API commit list endpoint; optional
+            "files_changed": len(commit.get("files", [])) if "files" in commit else None
+        })
+    return highlights
